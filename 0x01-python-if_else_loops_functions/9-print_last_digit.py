4ab3bc9ba3f2155584a24acd1fa82226f5cc9851
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-09-12 05:38:45 +0100

    Prints the last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,5 @@
 # print_last_digit - Prints last digit of a number
 def print_last_digit(number):
     r = abs(number) % 10
-    print("{:d}".format(r))
+    print("{:d}".format(r), end="")
     return r

fc6b19123332336de26c224aa27c3073b3006754
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-09-13 01:58:33 +0100

    Fix error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
-# This program imports a function from a module
+# Import the add function from the add_0 module
 from add_0 import add
+
 
 def main():
     a = 1
     b = 2
     result = add(1, 2)
     print("{} + {} = {}".format(a, b, result))
+
 
 if __name__ == "__main__":
     main()

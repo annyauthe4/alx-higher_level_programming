ce1d3c241663e0c12dc70c5d90e37223d6517818
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-09-13 12:39:00 +0100

    See any change

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 # Import the add function from the add_0 module
-from add_0 import add
+from add_0 import add as my_add
 
 
 def main():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, my_add(a, b)))
 
 
 if __name__ == "__main__":

16bd05d4b8ccc05f136fae6c6a16978cd5e9c51a
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-09-16 19:12:55 +0100

    Return multiple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,6 @@
 def multiple_returns(sentence):
     if sentence:
         length, first_char = len(sentence), sentence[0]
-        return length, first_char
+    else:
+        length, first_char = 0, None
+    return length, first_char

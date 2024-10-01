af6740e1ea1c3dea734e9a713ec6bcad8f0ee2dc
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-10-01 23:05:51 +0100

    Create Square class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines an empty class, square.
+   it does not allow the import of any module
+"""
+
+
+class Square:
+    """Square is an empty class. It currently does not
+       have any attributes or methods
+    """
+    pass

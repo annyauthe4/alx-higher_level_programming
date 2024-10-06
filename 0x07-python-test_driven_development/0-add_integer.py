d53b6f29dd5d7b95f83c06721c08215cf5cea34d
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-10-06 12:22:44 +0100

    Make changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,9 +10,10 @@ def add_integer(a, b=98):
     """Adds two integers or floats as integers
     Raises TypeError if inputs are invalid.
     """
-    if a is None:
-        raise TypeError("a must be an integer")
-    if not isinstance(a, (int, float)):
+    if len([a, b]) > 2:
+        raise TypeError("add_integer() takes from 1 to 2 positional \
+                        arguments but {} were given".format(len([a, b])))
+    if not isinstance(a, (int, float)) or a is None:
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")

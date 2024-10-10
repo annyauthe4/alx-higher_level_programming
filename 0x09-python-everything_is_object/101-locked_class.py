d257c74b492c253713ee6f374c2743ec68f0d833
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-10-10 05:50:07 +0100

    Lock class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""This module provides a class that locks the
+creation of an instance of all class attributes
+except the one indicated in `slots`
+"""
 class LockedClass:
+    """A locked class that prevents the creation
+    of any instance of the class attributes except
+    first name.
+    """
     __slots__ = ['first_name']
 
     def __init__(self):

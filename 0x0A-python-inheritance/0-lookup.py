f4f1736a3eaa6543a265ee2aa5891fc128bf80b1
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-10-14 21:57:04 +0100

    Return all object methods and attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module includes a function that returns the
+list of available attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    """Returns the attributes and methods of an object"""
+    return dir(obj)

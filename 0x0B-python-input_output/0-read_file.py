fe09f9cecad9274c8b7206fe46a8ab972b2ae66b
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-10-15 22:11:11 +0100

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """Reads and prints a text file"""
 
     with open(filename, 'r', encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")

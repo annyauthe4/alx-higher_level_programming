fa468e99d24585f4a75face2286587ba3e940261
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-10-15 22:07:42 +0100

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains function that opens file
+and ensure that it is closed even if error occured
+"""
+
+
+def read_file(filename=""):
+    """Reads and prints a text file"""
+
+    with open(filename, 'r', encoding="utf-8") as file:
+        print(file.read())

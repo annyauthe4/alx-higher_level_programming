7f6d6f87241a948a88d4548bc5ab95ec4d0d9f51
Otetumo Oluwaseun <annyauthe@yahoo.com>
2024-10-16 23:58:58 +0100

    Create base geometry class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module includes a class whose method is partially
+implemented and validated"""
+
+
+class BaseGeometry:
+    """A class exception raising method"""
+
+    def area(self):
+        """Raises an exception indicating that the area
+        method is not implemented.
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates integer.
+
+        Args:
+            name (str): user inputs.
+            value (int): user input
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: if value is less or equal to zero.
+        """
+
+        self.name = name
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0 or value is None:
+            raise ValueError("<name> must be greater than 0")
+        self.value = value

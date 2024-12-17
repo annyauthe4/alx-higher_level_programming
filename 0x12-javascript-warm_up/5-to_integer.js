f083e489fc7eeea2a4504775e3c0a2949b7ccb6a
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-17 02:57:06 +0100

    Convert to an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/*
+prints My number: <first argument converted in integer>
+if the first argument can be converted to an integer.
+*/
+const args = process.argv.slice(2);
+const num = Math.trunc(Number(args[0]));
+
+if (!isNaN(num)) {
+ console.log(`My number: ${num}`);
+} else {
+  console.log('Not a number');
+}

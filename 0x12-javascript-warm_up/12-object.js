97876aff06ec575da67b4355ba16f4622a821d6d
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-17 05:54:59 +0100

    Change object value

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+
+// Replace the value of 'value' property from 12 to 89
+myObject.value = 89;
+
+console.log(myObject);

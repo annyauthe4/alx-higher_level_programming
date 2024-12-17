dd8367948ba6bce1449e5444d8927b977e33e67c
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-17 03:33:27 +0100

    Print with loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
-// Prints 3 lines
 const a = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for (let i = 0; i < 3; i++) {
-  console.log(a[i]);
+for (const sentence of a) {
+  console.log(sentence);
 }

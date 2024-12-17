4fccbe55a2b74997129ac02c2ee568c3084b866a
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-17 03:27:21 +0100

    Parse int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-/*
-prints My number: <first argument converted in integer>
-if the first argument can be converted to an integer.
-*/
+
 const args = process.argv.slice(2);
 const num = parseInt(args[0]);
+
 if (!isNaN(num)) {
  console.log(`My number: ${num}`);
 } else {

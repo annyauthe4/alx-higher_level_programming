beea981a746b9bb2c6d179aad1b60e392ba47a07
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-19 05:37:54 +0100

    Add a constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/*
+Define a class Rectangle with a constructor. Then initialize the attributes.
+*/
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;

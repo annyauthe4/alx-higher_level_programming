ce5ee845985db471e8adb7648a6030fd2494d174
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-19 16:27:34 +0100

    Include rotate and print functions

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+/*
+Defines a Rectangle class with a constructor that initializes only if
+width and height properties are > 0. Prints Rectangle using X.
+Rotates it by exchanging the value of width with height.
+Doubles it by multiplying attributes by 2.
+*/
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;

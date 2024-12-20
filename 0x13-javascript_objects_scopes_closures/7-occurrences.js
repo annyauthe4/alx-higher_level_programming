eb5de826f618f311faf177b9bf2b94c1752fedd1
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-20 10:52:08 +0100

    Fix problem

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,5 +2,5 @@
 
 // Returns the number of occurrences in a list.
 exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((count, occurence) => (occurence === searchElement ? count + 1 : count), 0)
+  return list.reduce((count, occurence) => (occurence === searchElement ? count + 1 : count), 0);
 };

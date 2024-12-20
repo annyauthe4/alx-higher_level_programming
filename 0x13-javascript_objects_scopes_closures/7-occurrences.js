392b98da14365fd9ae541ae6b186c24364cb34fb
Oluwaseun Otetumo <annyauthe@yahoo.com>
2024-12-20 10:39:39 +0100

    Search engine

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+// Returns the number of occurrences in a list.
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((count, occurence) => (occurence === searchElement ? count + 1 : count), 0)
+};

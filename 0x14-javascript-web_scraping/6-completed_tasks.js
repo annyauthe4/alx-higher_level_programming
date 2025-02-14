421a781d347675a45b03d2d9c9d18504ce77c551
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-02-14 03:57:39 +0100

    Check if completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Computes the number of tasks completed by userId.
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, { json: true }, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
+});
